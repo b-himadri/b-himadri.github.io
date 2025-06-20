cede6bde1d307d68fa8bd21c4d76b876269f33b1
Himadri Barad <118922878+b-himadri@users.noreply.github.com>
2025-06-20 10:32:26 +0530

    adding file3.js

---
+++ javascript/file3.js
@@ -0,0 +1,16 @@
+let a = 10
+let b = 20 
+if(a>b){
+    console.log(`${a} is greater`)
+}
+else{
+    console.log(`${b} is greater`)
+}
+
+let x = 1;
+let y = 2;
+x>y ? console.log(`${x} is greater`) : console.log(`${y} is greater`);
+x>y && console.log(`${x} is greater`);
+
+let value = a||10
+console.log(value);
