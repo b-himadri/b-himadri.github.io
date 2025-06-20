7ad0d5d81434cccd9eebd50ab5d96c23a066723a
Himadri Barad <118922878+b-himadri@users.noreply.github.com>
2025-06-20 10:00:23 +0530

    Intro to Js

---
+++ javascript/file1.js
@@ -0,0 +1,15 @@
+//Introduction to JavaScript Variables and Data Types
+let studentName = "John";
+let age = 30;
+let promoted=true;
+let score
+let city=null
+console.log(studentName, age, promoted, score, city);
+console.log(typeof studentName)
+console.log(typeof age)
+let strAge = String(age); //use Number to convert string to number
+console.log(typeof strAge);
+
+let flag = "true";
+let booleanFlag = Boolean(flag); // Convert string to boolean
+console.log(booleanFlag, typeof booleanFlag);
---
+++ javascript/file2.js
@@ -0,0 +1,13 @@
+//Operations in JavaScript
+let a = 50
+let b = 20
+// console.log(a + b);
+// console.log(a - b); 
+// console.log(a * b); 
+// console.log(a / b); 
+console.log(a % b); 
+
+let c = 20
+let d = "3"
+console.log(c*d)
+console.log(a>b && a>c)
